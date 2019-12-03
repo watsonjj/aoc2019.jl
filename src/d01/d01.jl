28c31c51ed510c03aa4c02fa84dbc34c6ab5bc8f
watsonjj <jason.jw@live.co.uk>
2019-12-03 14:35:17 +0100

    d01 part2

--- src/d01/d01.jl
+++ src/d01/d01.jl
@@ -1,10 +1,15 @@
 using DelimitedFiles
 
-export fuel_required
+export fuel_required, fuel_required_including_fuel
 
-fuel_required(module_mass) = module_mass ÷ 3 - 2
+fuel_required(mass) = clamp(mass ÷ 3 - 2, 0, typemax(Int))
+
+function fuel_required_including_fuel(mass)
+    fuel = fuel_required(mass)
+    fuel == 0 ? 0 : fuel + fuel_required_including_fuel(fuel)
+end
 
 d01_inputpath = joinpath(@__DIR__, "input.txt")
 d01_inputread(path) = vec(readdlm(path, '\n', Int))
 d01_answer1(input) = sum(fuel_required, input)
-d01_answer2(input) = NaN
+d01_answer2(input) = sum(fuel_required_including_fuel, input)
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,13 @@ end
     @test fuel_required(14) == 2
     @test fuel_required(1969) == 654
     @test fuel_required(100756) == 33583
+    @test fuel_required(2) == 0
+end
+
+@testset "fuel_required_including_fuel" begin
+    @test fuel_required_including_fuel(14) == 2
+    @test fuel_required_including_fuel(1969) == 966
+    @test fuel_required_including_fuel(100756) == 50346
 end
 
 @testset "d01_answers" begin
@@ -19,5 +26,5 @@ end
     input = aoc2019.d01_inputread(aoc2019.d01_inputpath)
     @test length(input) == 100
     @test aoc2019.d01_answer1(input) == 3334297
-    @test aoc2019.d01_answer2(input) == 3334297
+    @test aoc2019.d01_answer2(input) == 4998565
 end
